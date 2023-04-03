506e1c33178640bcf0fa73c7cfa884792aac913f
jaehyoek <jhlim244@naver.com>
2023-04-03 09:43:41 +0900

    commit function name

--- Plant_Diary.c
+++ Plant_Diary.c
@@ -1,3 +1,23 @@
+#include "Text_Option.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+#define MAX_LENGTH 1000
+
+typedef struct Text_Name
+{
+    char name[16];
+    struct Text_Name *next
+} Text_Name;
+
+int main()
+{
+    char description[MAX_LENGTH] = "";
+
+    while (1)
+    {
+        description[0] = '\0';
+    }
+}
