8a8715bd0af6a9efb2542f218d0fba111ca7170c
andrew.Han <kihan@snut.ac.kr>
2023-04-03 17:10:10 +0900

    makefile-build폴더로 변경

--- src/NodeManage.c
+++ src/NodeManage.c
@@ -5,10 +5,7 @@ Node *head = NULL;
 // 노드 추가 함수
 void Add(char *name)
 {
-    Node *new_node = (Node *)malloc(sizeof(Node));
-    new_node->name = (char *)malloc(strlen(name) + 1);
-    strcpy(new_node->next, name) = head;
-    head = new_node;
+
 }
 
 // 노드 삭제 함수
@@ -22,7 +19,7 @@ void List()
 }
 
 // 원하는 정보 찾는 함수
-Text_Name *Search()
+void *Search()
 {
 }
 
