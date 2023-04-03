3ff6fd4c16efd62625c401631df307e57b9f3922
jaehyoek <jhlim244@naver.com>
2023-04-03 12:17:23 +0900

    commit function name

---
+++ NodeManage.c
@@ -0,0 +1,26 @@
+#include "MakeText.h"
+
+// 노드 추가 함수
+void Add()
+{
+}
+
+// 노드 삭제 함수
+void Delete()
+{
+}
+
+// 리스트 출력해주는 함수
+void List()
+{
+}
+
+// 원하는 정보 찾는 함수
+Text_Name *Search()
+{
+}
+
+// txt 파일 편집하는 함수
+void txt_Edit()
+{
+}
