1f03afc7325cf6b84d61a3b91de56e9432a130c5
Ggchibal <129123422+Ggchibal@users.noreply.github.com>
2023-04-05 19:41:59 +0900

    Add files via upload
    파일 업로드 수정, 함수 추가, 주석 추후 추가 예정

---
+++ Plant_Diary/src/MakeText.c
@@ -0,0 +1,146 @@
+#include "MakeText.h"
+#include "Plant_Diary.c" //MAX_LEGTH 1000 연결
+
+// 첫 페이지 프린트 하는 함수
+void Print_Title(void)
+{
+    char diary[] = {"식물 일기장"};
+
+    puts(" ");
+    printf("\t\t%s\n", diary);
+    puts(" ");
+
+    puts("1. 입력하기");
+    puts("2. 끝내기");
+}
+
+// 폴더 페이지 프린트하는 함수
+void Print_folder(void)
+{
+    char diary[] = {"식물 일기장"};
+
+    puts(" ");
+    printf("\t\t%s\n", diary);
+    puts(" ");
+
+    puts("1. 폴더 추가");
+    puts("2. 폴더 삭제");
+    puts("3. 이전 페이지\n\n");
+}
+
+// 텍스트를 프린트하는 함수
+void Print_Text(char *folder_name)
+{
+
+    puts(" ");
+    printf("\t\t%s\n", &folder_name);
+    puts(" ");
+
+    puts("1. 일기 작성");
+    puts("2. 일기 삭제");
+    puts("3. 처음 화면\n\n");
+}
+
+// 파일 이름 정하는 함수
+// time_t #include <time.h>을 참조하는 구조체를 기준으로 파일 이름 자동 출력
+int Get_Date_File_Name(void)
+{
+    int year, month, day;
+    struct tm *t;
+    time_t timer;
+
+    timer = time(NULL);
+    t = localtime(&timer);
+
+    year = t->tm_year + 1900;
+    month = t->tm_mon + 1;
+    day = t->tm_mday;
+
+    return (year * 10000 + month * 100 + day);
+}
+
+// 일기 내용 입력하는 함수
+void Scan_Description(char description[])
+{
+    int ch;
+    unsigned int index = 0;
+    char guide[] = "Enter 입력 및 MAX_LENGTH 까지 입력 시 종료";
+
+    getchar();
+    printf("%s(0/%d)\n", guide, MAX_LENGTH - 1);
+
+    //getchar() 내 enter(\n) 입력, MAX_LENGTH 초과, EOF 일 때 입력 종료
+    while ((ch = getchar()) != EOF)
+    {
+        description[index] = ch;
+        index = strlen(description);
+
+        system("clear");
+
+        printf(
+            "%s(%d/%d):%s",
+            guide, index,
+            MAX_LENGTH - 1, description);
+
+        if (index + 1 == MAX_LENGTH - 1)
+        {
+            printf("\nMAX_LENGTH 초과 저장 후 종료");
+            break;
+        }
+        else if(ch = getcahr() != '\n')
+        {
+            printf("\nEnter 입력, 저장 후 종료");
+            break;
+        }
+    }
+
+    description[index + 1] = '\0';
+}
+
+// 일기를 txt 파일에 저장하는 함수
+// txt 파일을 지정한 폴더에 저장할 수 있도록 수정//
+void Put_Diary(int date, char description[])
+{
+    char filename[20];//문자열 변수 설정
+    FILE *fp = NULL;//FILE 포인터 변수 선언
+
+    sprintf(filename, "%d", date);//filename에 날짜 입력
+    strcat(filename, ".txt");//strcat 함수로 붙여넣기
+    fp = fopen(filename, "w");//file열어 작성하기
+    fprintf(fp, "%s", description);//내용 작성
+    fclose(fp);
+}
+
+// 일기를 txt 파일로부터 가져오는 함수
+int Get_Diary(int date, char description[])
+{
+    char filename[20];
+    FILE *fp;
+
+    sprintf(filename, "%d", date);
+    strcat(filename, ".txt"); //자료형 파일과 문자 조합 함수
+    fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        return 0;
+    }
+
+    fgets(description, MAX_LENGTH, fp);
+    fclose(fp);
+
+    return 1;
+}
+
+// 폴더 만드는 함수
+void Make_Folder(char name)
+{ /* 폴더 이름을 입력 받아 배열에 저장하고 그 문자열로 텍스트파일 저장
+     할 수 있도록 하기*/
+    int result = mkdir(name, 0777);
+    if (result == -1)
+    {
+        printf("폴더 생성 실패.\n");
+        return -1;
+    }
+    printf("%s 폴더 생성 성공.\n");
+}
---
+++ Plant_Diary/src/MakeText.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h> //File list
+
+typedef struct Node
+{
+    char *name;
+    struct Node *next;
+} Node;
+
+void Print_Title();
+int Get_Date_File_Name();
+void Scan_Description(char[]);
+void Put_Diary(int, char[]);
+int Get_Diary(int, char[]);
+void Add(char *);
+void Delete(char *);
+void List();
+// void txt_Edit();
