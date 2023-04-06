28251bf248b8c403bf061b9dcfe56ccd9a739502
jaehyoek <jhlim244@naver.com>
2023-04-06 13:34:18 +0900

    fix

--- src/MakeText.c
+++ src/MakeText.c
@@ -2,23 +2,15 @@
 
 #define MAX_LENGTH 1000
 
-#define MAX_LENGTH 1000
-
 // 첫 페이지 프린트 하는 함수
 void Print_Title(void)
 {
     char diary[] = {"식물 일기장"};
-    char diary[] = {"식물 일기장"};
 
     puts(" ");
     printf("\t\t%s\n", diary);
     puts(" ");
-    puts(" ");
-    printf("\t\t%s\n", diary);
-    puts(" ");
 
-    puts("1. 입력하기");
-    puts("2. 끝내기");
     puts("1. 입력하기");
     puts("2. 끝내기");
 }
@@ -27,11 +19,7 @@ void Print_Title(void)
 void Print_folder(void)
 {
     char diary[] = {"식물 일기장"};
-    char diary[] = {"식물 일기장"};
 
-    puts(" ");
-    printf("\t\t%s\n", diary);
-    puts(" ");
     puts(" ");
     printf("\t\t%s\n", diary);
     puts(" ");
@@ -46,9 +34,6 @@ void Print_Text(char *folder_name)
     puts(" ");
     printf("\t\t%s\n", folder_name);
     puts(" ");
-    puts(" ");
-    printf("\t\t%s\n", folder_name);
-    puts(" ");
 
     puts("1. 일기 작성\t2. 일기 삭제\t3. 이전 화면\n\n");
 }
@@ -60,23 +45,14 @@ int Get_Date_File_Name(void)
     int year, month, day;
     struct tm *t;
     time_t timer;
-    int year, month, day;
-    struct tm *t;
-    time_t timer;
 
-    timer = time(NULL);
-    t = localtime(&timer);
     timer = time(NULL);
     t = localtime(&timer);
 
     year = t->tm_year + 1900;
     month = t->tm_mon + 1;
     day = t->tm_mday;
-    year = t->tm_year + 1900;
-    month = t->tm_mon + 1;
-    day = t->tm_mday;
 
-    return (year * 10000 + month * 100 + day);
     return (year * 10000 + month * 100 + day);
 }
 
@@ -86,12 +62,7 @@ void Scan_Description(char description[])
     int ch;
     unsigned int index = 0;
     char guide[] = "Enter 입력 및 MAX_LENGTH 까지 입력 시 종료";
-    int ch;
-    unsigned int index = 0;
-    char guide[] = "Enter 입력 및 MAX_LENGTH 까지 입력 시 종료";
 
-    getchar();
-    printf("%s(0/%d)\n", guide, MAX_LENGTH - 1);
     getchar();
     printf("%s(0/%d)\n", guide, MAX_LENGTH - 1);
 
@@ -100,35 +71,13 @@ void Scan_Description(char description[])
     {
         description[index] = ch;
         index = strlen(description);
-        // getchar() 내 enter(\n) 입력, MAX_LENGTH 초과, EOF 일 때 입력 종료
-        while ((ch = getchar()) != EOF)
-        {
-            description[index] = ch;
-            index = strlen(description);
 
-            system("clear");
-            system("clear");
+        system("clear");
+        printf(
+            "%s(%d/%d):%s",
+            guide, index,
+            MAX_LENGTH - 1, description);
 
-            printf(
-                "%s(%d/%d):%s",
-                guide, index,
-                MAX_LENGTH - 1, description);
-            printf(
-                "%s(%d/%d):%s",
-                guide, index,
-                MAX_LENGTH - 1, description);
-
-            if (index + 1 == MAX_LENGTH - 1)
-            {
-                printf("\nMAX_LENGTH 초과 저장 후 종료");
-                break;
-            }
-            else if ((ch = getchar()) != '\n')
-            {
-                printf("\nEnter 입력, 저장 후 종료");
-                break;
-            }
-        }
         if (index + 1 == MAX_LENGTH - 1)
         {
             printf("\nMAX_LENGTH 초과 저장 후 종료");
